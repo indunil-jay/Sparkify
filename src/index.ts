fe8bc4896a3406d2dd7ba6d0e4473236b9813b58
indunil-jay <indunil.jayz@gmail.com>
2024-05-04 17:31:32 +0530

    add menu fade functionality

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,26 @@ toggleCloseBtn.addEventListener("click", () => {
   toggleOpenBtn.classList.add("header__icon--menu--active");
   mobileNavigation.dataset.expanded = "false";
 });
+
+//menu fade when hover item.
+const headerParent = document.querySelector<HTMLHeadElement>(".header")!;
+
+const handleHover = (e: Event, opacity: number) => {
+  if ((e.target as HTMLElement).classList.contains("header__link")) {
+    const link = e.target as HTMLElement;
+    const siblings = link.closest(".header")!.querySelectorAll(".header__link");
+
+    const header = link.closest(".header") as HTMLElement;
+    const logo = header.querySelector(".header__left")! as HTMLElement;
+    const btns = header.querySelector(".header__right")! as HTMLElement;
+
+    siblings.forEach((el) => {
+      const sibling = el as HTMLElement;
+      if (sibling !== link) sibling.style.opacity = opacity.toString();
+    });
+    logo.style.opacity = opacity.toString();
+    btns.style.opacity = opacity.toString();
+  }
+};
+headerParent.addEventListener("mouseover", (e: Event) => handleHover(e, 0.5));
+headerParent.addEventListener("mouseout", (e: Event) => handleHover(e, 1));
